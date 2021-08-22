d572eb893ced0cd2a1b8c97f55bc473b673b114b
Johan Jacobs <20131902+JohanJacobs@users.noreply.github.com>
2021-08-23 07:06:27 +1200

    Chapter 12, Pattern of Patterns Duck simulator project created.

---
+++ Chapter12_Patterns_of_Patterns_DuckSim/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello Head First Design Patterns!\n";
+	std::cout << "Chapter 12 - Patterns of Patterns!\n";
+	std::cout << "Duck simluator\n";
+}
